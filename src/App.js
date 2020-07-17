22214f4fdc2b0d9ddb78bb875c970b69a230cd71
Anthony Comanzo <33241429+acomanzo@users.noreply.github.com>
2020-07-16 20:58:32 -0400

    Update App.js
    Fixed the issues I marked (mostly). Namely, fixed the rangesOverlap issue and handling the "what can I catch right now?" feature, except for some reason one of the toggle buttons doesn't work but I suspect it is the library's fault and not mine.

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ class App extends Component {
   updateList() {
     this.setState({
       items: this.props.items
-    })
+    });
   }
 
   fetchItems() {
@@ -142,7 +142,7 @@ class App extends Component {
           filteredItems: data
         })
       }.bind(this))
-      .catch(error => console.log("Fetch error: " + error))
+      .catch(error => console.log("Fetch error: " + error));
 
     //let requestURL = '/Users/anthony/Desktop/Coding\ Stuff/React/big-money/src/stock.json';
     /*let requestURL = 'http://localhost:3000/stock';
@@ -168,7 +168,7 @@ class App extends Component {
   toggleBugs() {
     this.setState(prevState => ({
       displayBugs: !prevState.displayBugs
-    }))
+    }));
   }
 
   toggleFish() {
@@ -176,7 +176,7 @@ class App extends Component {
       displayFish: !prevState.displayFish
     }), () => {
 
-    })
+    });
   }
 
   updateFromTime() {
@@ -224,18 +224,18 @@ class App extends Component {
   }
 
   updateMonths(event) {
-    var temp = this.state.months
-    const value = Number(event.target.value)
-    const index = temp.indexOf(value)
+    var temp = this.state.months;
+    const value = Number(event.target.value);
+    const index = temp.indexOf(value);
     if (index === -1) {
-      temp.push(value)
+      temp.push(value);
     }
     else {
       temp.splice(index, 1);
     }
     this.setState({
       months: temp
-    })
+    });
   }
 
   /**
@@ -250,70 +250,35 @@ class App extends Component {
    * @returns {boolean} true if the ranges overlap, false otherwise
    */
   rangesOverlap(from1, to1, from2, to2) {
-
-    from1 = from1.toLowerCase();
-    to1 = to1.toLowerCase();
+    console.log(from1 + ", " + to1 + "--" + from2 + ", " + to2);
     from2 = from2.toLowerCase();
     to2 = to2.toLowerCase();
 
-    // for each time, cut out am/pm and convert to military time. If a time
-    // updates to 24, switch it to 0
-    if (from1.indexOf("pm") !== -1) {
-      from1 = Number(from1.slice(0, -2).trim()) + 12
-      if (from1 === 24) {
-        from1 = 0
-      }
-    } else {
-      from1 = Number(from1.slice(0, -2).trim())
-    }
-    if (to1.indexOf("pm") !== -1) {
-      to1 = Number(to1.slice(0, -2).trim()) + 12
-      if (to1 === 24) {
-        to1 = 0
-      }
-    } else {
-      to1 = Number(to1.slice(0, -2).trim())
-    }
+    // for the second set, cut out am/pm and convert to military time.
+    // If "pm" is in the string, add 12. If a time updates to 24, revert it
+    // to 12
     if (from2.indexOf("pm") !== -1) {
-      from2 = Number(from2.slice(0, -2).trim()) + 12
+      from2 = Number(from2.slice(0, -2).trim()) + 12;
       if (from2 === 24) {
-        from2 = 0
+        from2 = 12;
       }
     } else {
-      from2 = Number(from2.slice(0, -2).trim())
+      from2 = Number(from2.slice(0, -2).trim());
     }
     if (to2.indexOf("pm") !== -1) {
-      to2 = Number(to2.slice(0, -2).trim()) + 12
+      to2 = Number(to2.slice(0, -2).trim()) + 12;
       if (to2 === 24) {
-        to2 = 0
+        to2 = 12;
       }
     } else {
-      to2 = Number(to2.slice(0, -2).trim())
+      to2 = Number(to2.slice(0, -2).trim());
     }
 
-    if (from1 === from2 && to1 === to2) {
-      return true;
-    }
-    else if (from1 === to2 && to1 === from2) {
-      return false;
-    }
-    else if (from1 >= to2 && from2 >= to1) {
-      return false;
-    }
-    else if (from1 < from2 && to1 <= from2 && from1 < to2 && to1 <= to2) {
-      if (from2 < 12) {
-        return false;
-      }
-    }
-    else if (from1 >= to2 && to1 > to2 && from1 > from2 && to1 > from2) {
-      if (from2 > 12) {
-        return true;
-      }
-      return false;
-
-    }
-    return true;
-
+    // see proof on stack overflow
+    // "Algorithm to detect overlapping periods [duplicate]"
+    // "Determine Whether Two Date Ranges Overlap"
+    // use > and < to prevent adjacent/touching ranges from matching
+    return from1 < to2 && from2 < to1;
   }
 
   /**
@@ -324,75 +289,82 @@ class App extends Component {
    */
   cook() {
     if (this.state.items == null) {
-      return
+      return;
     }
-    var temp = this.state.items
-    const fromTime = this.state.fromTime
-    const toTime = this.state.toTime
+    var temp = this.state.items;
+    const fromTime = this.state.fromTime;
+    const toTime = this.state.toTime;
 
     var self = this; // for the callback
     // if bugs are toggled on or fish are toggled on and this animal is a
     // bug or a fish, return true
     temp = temp.filter(function(animal) {
-      var matchBugsToggle = false
-      var matchFishToggle = false
-      const displayBugs = self.state.displayBugs
-      const displayFish = self.state.displayFish
+      var matchBugsToggle = false;
+      var matchFishToggle = false;
+      const displayBugs = self.state.displayBugs;
+      const displayFish = self.state.displayFish;
       // if both are toggled on, display all fish and bugs
       if (displayBugs && displayFish) {
-        matchBugsToggle = true
-        matchFishToggle = true
+        matchBugsToggle = true;
+        matchFishToggle = true;
       }
       // if bugs are toggled on and the animal is a bug, display it
       else if (displayBugs && (typeof animal.shadow_size === 'undefined')) {
-        matchBugsToggle = true
+        matchBugsToggle = true;
       }
       // if the fish are toggled on and the animal is a fish, display it
       else if (displayFish && (typeof animal.shadow_size !== 'undefined')) {
-        matchFishToggle = true
+        matchFishToggle = true;
       }
 
-      return matchBugsToggle || matchFishToggle
+      return matchBugsToggle || matchFishToggle;
     })
     // if this animal passes the time and month check, return true
     temp = temp.filter(function(animal) {
-      var matchTime = false
-      var matchMonths = false
+      var matchTime = false;
+      var matchMonths = false;
 
       if (animal.time.localeCompare("All day") === 0) {
         matchTime = true;
       }
       else {
         // get an array of the animal's time field
-        const animalTime = animal.time.split(" ")
+        const animalTime = animal.time.split(" ");
         // make a new array such that the first index is the starting time
         // and the second index is the ending time, including AM/PM
-        const animalTimeArray = [animalTime[0].concat(animalTime[1]).toLowerCase(),
-                                 animalTime[3].concat(animalTime[4]).toLowerCase()]
-        matchTime = self.rangesOverlap(fromTime, toTime, animalTimeArray[0], animalTimeArray[1])
+        var animalTimeArray = [animalTime[0].concat(animalTime[1]).toLowerCase(),
+                                 animalTime[3].concat(animalTime[4]).toLowerCase()];
+        matchTime = self.rangesOverlap(fromTime, toTime, animalTimeArray[0], animalTimeArray[1]);
+        // dont forget about the case where there are two times an animal
+        // could appear
+        if (animalTime.includes("&")) {
+          animalTimeArray[0] = animalTime[6].concat(animalTime[7]);
+          animalTimeArray[1] = animalTime[9].concat(animalTime[10]);
+          matchTime = matchTime || self.rangesOverlap(fromTime, toTime, animalTimeArray[0], animalTimeArray[1]);
+        }
       }
 
       // if the months chosen are a subset of this animal's set of months,
       // display it
       switch (self.state.hemisphere) {
         case "northern":
-          matchMonths = self.state.months.every(val => animal.months.northern.includes(val))
+          matchMonths = self.state.months.every(val => animal.months.northern.includes(val));
           break;
         case "southern":
-          matchMonths = self.state.months.every(val => animal.months.southern.includes(val))
+          matchMonths = self.state.months.every(val => animal.months.southern.includes(val));
           break;
         default:
-          alert("error cooking")
+          alert("error cooking");
           break;
       }
-      return matchTime && matchMonths
-    })
+      return matchTime && matchMonths;
+    });
 
     this.setState({
       filteredItems: temp
     }, () => {
-      console.log("Cook callback:")
-    })
+      console.log("Cook callback:");
+    });
   }
 
   handleSearch(event) {
@@ -404,54 +376,142 @@ class App extends Component {
       currentList = this.state.items;
 
       newList = currentList.filter(item => {
-        const lcItem = item.name.toLowerCase()
+        const lcItem = item.name.toLowerCase();
 
-        const filter = event.target.value.toLowerCase()
+        const filter = event.target.value.toLowerCase();
 
         return lcItem.includes(filter);
-      })
+      });
     } else {
       newList = this.state.filteredItems;
     }
 
     this.setState({
       filteredItems: newList
-    })
+    });
   }
 
   handleWhatCanICatchNow() {
-    const date = new Date()
-    var hour = date.getHours()
-    this.setState({
-      months: [date.getMonth()]
-    })
-    if (hour >= 12) {
-      hour = hour - 12
-      this.setState({
-        fromTime: hour + "pm"
-      })
-      this.setState({
-        toTime: (hour + 1) + "pm"
-      })
-    } else {
-      this.setState({
-        fromTime: hour + "am"
-      })
-      this.setState({
-        toTime: (hour + 1) + "am"
-      })
+    const date = new Date();
+    const hour = date.getHours(); // expect 0 - 23
+    const month = date.getMonth(); // expect 0 - 11
+    // using .click() will mimic the user actually clicking
+    // and even call functions
+    var monthButton = document.getElementById("months-group").children[month];
+    monthButton.click();
+    var fromTimeElement = document.getElementById("from-time");
+    var toTimeElement = document.getElementById("to-time");
+    switch(hour) {
+      case 0:
+        fromTimeElement.selectedIndex = 0;
+        toTimeElement.selectedIndex = 1;
+        break;
+      case 1:
+        fromTimeElement.selectedIndex = 1;
+        toTimeElement.selectedIndex = 2;
+        break;
+      case 2:
+        fromTimeElement.selectedIndex = 2;
+        toTimeElement.selectedIndex = 3;
+        break;
+      case 3:
+        fromTimeElement.selectedIndex = 3;
+        toTimeElement.selectedIndex = 4;
+        break;
+      case 4:
+        fromTimeElement.selectedIndex = 4;
+        toTimeElement.selectedIndex = 5;
+        break;
+      case 5:
+        fromTimeElement.selectedIndex = 5;
+        toTimeElement.selectedIndex = 6;
+        break;
+      case 6:
+        fromTimeElement.selectedIndex = 6;
+        toTimeElement.selectedIndex = 7;
+        break;
+      case 7:
+        fromTimeElement.selectedIndex = 7;
+        toTimeElement.selectedIndex = 8;
+        break;
+      case 8:
+        fromTimeElement.selectedIndex = 8;
+        toTimeElement.selectedIndex = 9;
+        break;
+      case 9:
+        fromTimeElement.selectedIndex = 9;
+        toTimeElement.selectedIndex = 10;
+        break;
+      case 10:
+        fromTimeElement.selectedIndex = 10;
+        toTimeElement.selectedIndex = 11;
+        break;
+      case 11:
+        fromTimeElement.selectedIndex = 11;
+        toTimeElement.selectedIndex = 12;
+        break;
+      case 12:
+        fromTimeElement.selectedIndex = 12;
+        toTimeElement.selectedIndex = 13;
+        break;
+      case 13:
+        fromTimeElement.selectedIndex = 13;
+        toTimeElement.selectedIndex = 14;
+        break;
+      case 14:
+        fromTimeElement.selectedIndex = 14;
+        toTimeElement.selectedIndex = 15;
+        break;
+      case 15:
+        fromTimeElement.selectedIndex = 15;
+        toTimeElement.selectedIndex = 16;
+        break;
+      case 16:
+        fromTimeElement.selectedIndex = 16;
+        toTimeElement.selectedIndex = 17;
+        break;
+      case 17:
+        fromTimeElement.selectedIndex = 17;
+        toTimeElement.selectedIndex = 18;
+        break;
+      case 18:
+        fromTimeElement.selectedIndex = 18;
+        toTimeElement.selectedIndex = 19;
+        break;
+      case 19:
+        fromTimeElement.selectedIndex = 19;
+        toTimeElement.selectedIndex = 20;
+        break;
+      case 20:
+        fromTimeElement.selectedIndex = 20;
+        toTimeElement.selectedIndex = 21;
+        break;
+      case 21:
+        fromTimeElement.selectedIndex = 21;
+        toTimeElement.selectedIndex = 22;
+        break;
+      case 22:
+        fromTimeElement.selectedIndex = 22;
+        toTimeElement.selectedIndex = 23;
+        break;
+      case 23:
+        fromTimeElement.selectedIndex = 23;
+        toTimeElement.selectedIndex = 0;
+        break;
     }
+    this.updateFromTime();
+    this.updateToTime();
+    document.getElementById("northernButton").click();
+    document.getElementById('toggle-bugs-button').click();
+    document.getElementById('toggle-fish-button').click();
+
     this.setState({
-      hemisphere: "northern"
-    })
-    this.setState({
-      displayBugs: true
-    })
-    this.setState({
-      displayFish: true
+      // add a setState to the queue so we can make sure we cook once
+      // everything up top is done updating
     }, () => {
-      this.cook()
-    })
+      this.cook();
+    });
+
   }
 
   componentDidMount() {
@@ -466,71 +526,71 @@ class App extends Component {
         </header>
         <div id="hudContainer">
           <ToggleButtonGroup type="checkbox" className="mb-1 flex-wrap">
-            <ToggleButton value={1} onChange={this.toggleBugs}>Bugs</ToggleButton>
-            <ToggleButton value={2} onChange={this.toggleFish}>Fish</ToggleButton>
+            <ToggleButton id="toggle-bugs-button" value={1} onChange={this.toggleBugs}>Bugs</ToggleButton>
+            <ToggleButton id="toggle-fish-button" value={2} onChange={this.toggleFish}>Fish</ToggleButton>
           </ToggleButtonGroup>
           <br />
           <label>From when?</label>
           <select id="from-time" onChange={this.updateFromTime}>
             <optgroup label="Morning (AM)">
-              <option value="12am">12:00</option>
-              <option value="1am">1:00</option>
-              <option value="2am">2:00</option>
-              <option value="3am">3:00</option>
-              <option value="4am">4:00</option>
-              <option value="5am">5:00</option>
-              <option value="6am">6:00</option>
-              <option value="7am">7:00</option>
-              <option value="8am">8:00</option>
-              <option value="9am">9:00</option>
-              <option value="10am">10:00</option>
-              <option value="11am">11:00</option>
+              <option value="0">12:00</option>
+              <option value="1">1:00</option>
+              <option value="2">2:00</option>
+              <option value="3">3:00</option>
+              <option value="4">4:00</option>
+              <option value="5">5:00</option>
+              <option value="6">6:00</option>
+              <option value="7">7:00</option>
+              <option value="8">8:00</option>
+              <option value="9">9:00</option>
+              <option value="10">10:00</option>
+              <option value="11">11:00</option>
             </optgroup>
             <optgroup label="Afternoon (PM)">
-              <option value="12pm">12:00</option>
-              <option value="1pm">1:00</option>
-              <option value="2pm">2:00</option>
-              <option value="3pm">3:00</option>
-              <option value="4pm">4:00</option>
-              <option value="5pm">5:00</option>
-              <option value="6pm">6:00</option>
-              <option value="7pm">7:00</option>
-              <option value="8pm">8:00</option>
-              <option value="9pm">9:00</option>
-              <option value="10pm">10:00</option>
-              <option value="11pm">11:00</option>
+              <option value="12">12:00</option>
+              <option value="13">1:00</option>
+              <option value="14">2:00</option>
+              <option value="15">3:00</option>
+              <option value="16">4:00</option>
+              <option value="17">5:00</option>
+              <option value="18">6:00</option>
+              <option value="19">7:00</option>
+              <option value="20">8:00</option>
+              <option value="21">9:00</option>
+              <option value="22">10:00</option>
+              <option value="23">11:00</option>
             </optgroup>
           </select>
           <br />
           <label>Until when?</label>
           <select id="to-time" onChange={this.updateToTime}>
             <optgroup label="Morning (AM)">
-              <option value="12am">12:00</option>
-              <option value="1am">1:00</option>
-              <option value="2am">2:00</option>
-              <option value="3am">3:00</option>
-              <option value="4am">4:00</option>
-              <option value="5am">5:00</option>
-              <option value="6am">6:00</option>
-              <option value="7am">7:00</option>
-              <option value="8am">8:00</option>
-              <option value="9am">9:00</option>
-              <option value="10am">10:00</option>
-              <option value="11am">11:00</option>
+              <option value="0">12:00</option>
+              <option value="1">1:00</option>
+              <option value="2">2:00</option>
+              <option value="3">3:00</option>
+              <option value="4">4:00</option>
+              <option value="5">5:00</option>
+              <option value="6">6:00</option>
+              <option value="7">7:00</option>
+              <option value="8">8:00</option>
+              <option value="9">9:00</option>
+              <option value="10">10:00</option>
+              <option value="11">11:00</option>
             </optgroup>
             <optgroup label="Afternoon (PM)">
               <option value="12pm">12:00</option>
-              <option value="1pm">1:00</option>
-              <option value="2pm">2:00</option>
-              <option value="3pm">3:00</option>
-              <option value="4pm">4:00</option>
-              <option value="5pm">5:00</option>
-              <option value="6pm">6:00</option>
-              <option value="7pm">7:00</option>
-              <option value="8pm">8:00</option>
-              <option value="9pm">9:00</option>
-              <option value="10pm">10:00</option>
-              <option value="11pm">11:00</option>
+              <option value="13">1:00</option>
+              <option value="14">2:00</option>
+              <option value="15">3:00</option>
+              <option value="16">4:00</option>
+              <option value="17">5:00</option>
+              <option value="18">6:00</option>
+              <option value="19">7:00</option>
+              <option value="20">8:00</option>
+              <option value="21">9:00</option>
+              <option value="22">10:00</option>
+              <option value="23">11:00</option>
             </optgroup>
           </select>
           <br />
@@ -539,7 +599,7 @@ class App extends Component {
             <ToggleButton id="southernButton" value={2} onChange={this.toggleSouthernHemisphere}>Southern Hemisphere</ToggleButton>
           </ToggleButtonGroup>
           <br />
-          <ToggleButtonGroup type="checkbox" className="mb-1 flex-wrap">
+          <ToggleButtonGroup id="months-group" type="checkbox" className="mb-1 flex-wrap">
             <ToggleButton value={1} onChange={this.updateMonths}>Jan</ToggleButton>
             <ToggleButton value={2} onChange={this.updateMonths}>Feb</ToggleButton>
             <ToggleButton value={3} onChange={this.updateMonths}>Mar</ToggleButton>
