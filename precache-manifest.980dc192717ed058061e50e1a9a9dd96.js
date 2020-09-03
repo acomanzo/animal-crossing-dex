0ea4b6db82577cd9a0c757c0923d70064d920dd0
acomanzo <ansonih17@gmail.com>
2020-09-03 11:52:06 -0400

    Updates

---
+++ precache-manifest.980dc192717ed058061e50e1a9a9dd96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d035825d61f9b7e38aeec3e6c603aed5",
+    "url": "/animal-crossing-dex/index.html"
+  },
+  {
+    "revision": "ff6e3488c3b938d629de",
+    "url": "/animal-crossing-dex/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "afa650f6134895396d2c",
+    "url": "/animal-crossing-dex/static/css/main.1f2185d8.chunk.css"
+  },
+  {
+    "revision": "ff6e3488c3b938d629de",
+    "url": "/animal-crossing-dex/static/js/2.9bb8f2e3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/animal-crossing-dex/static/js/2.9bb8f2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa650f6134895396d2c",
+    "url": "/animal-crossing-dex/static/js/main.a70d4646.chunk.js"
+  },
+  {
+    "revision": "a75c5201bca8a0db21e1",
+    "url": "/animal-crossing-dex/static/js/runtime-main.c35267a7.js"
+  }
+]);
